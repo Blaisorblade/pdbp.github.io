4f9fea1cda23cd94db533970fec8a303591369eb
Luc Duponcheel <luc@strongtyped.io>
2018-08-06 18:14:28 +0200

    commit before refactoring main programs

---
+++ src/main/scala/examples/objects/active/effectfulReadingAndWriting/effectfulUtils.scala
@@ -0,0 +1,23 @@
+package examples.objects.active.effectfulReadingAndWriting
+
+//       _______         __    __        _______
+//      / ___  /\       / /\  / /\      / ___  /\
+//     / /__/ / / _____/ / / / /_/__   / /__/ / /
+//    / _____/ / / ___  / / / ___  /\ /____  / /
+//   / /\____\/ / /__/ / / / /__/ / / \___/ / /
+//  /_/ /      /______/ / /______/ /     /_/ /
+//  \_\/       \______\/  \______\/      \_\/
+//                                           v1.0
+//  Program Description Based Programming Library
+//  author        Luc Duponcheel        2017-2018
+
+import pdbp.types.active.activeTypes._
+
+import pdbp.program.implicits.active.implicits
+import implicits.activeProgram
+
+// import examples.mainPrograms.effectfulReadingAndWriting.MainFactorial
+
+import examples.utils.EffectfulUtils
+
+object effectfulUtils extends EffectfulUtils[`=>A`]()
